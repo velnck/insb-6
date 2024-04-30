3077862ec08c9c2802bf22e52d81fd098d4c0b38
velnck <v.veronika.sidorko@gmail.com>
2024-04-30 05:43:05 +0300

    installer for app

--- lab4/App/Forms/MainForm.Designer.cs
+++ lab4/App/Forms/MainForm.Designer.cs
@@ -28,75 +28,79 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.logInButton = new System.Windows.Forms.Button();
-			this.usernameTextBox = new System.Windows.Forms.TextBox();
-			this.passwordTextBox = new System.Windows.Forms.TextBox();
-			this.usernameLabel = new System.Windows.Forms.Label();
-			this.passwordLabel = new System.Windows.Forms.Label();
-			this.SuspendLayout();
+			logInButton = new Button();
+			usernameTextBox = new TextBox();
+			passwordTextBox = new TextBox();
+			usernameLabel = new Label();
+			passwordLabel = new Label();
+			SuspendLayout();
 			// 
 			// logInButton
 			// 
-			this.logInButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-			this.logInButton.Location = new System.Drawing.Point(353, 302);
-			this.logInButton.Name = "logInButton";
-			this.logInButton.Size = new System.Drawing.Size(102, 37);
-			this.logInButton.TabIndex = 0;
-			this.logInButton.Text = "Log In";
-			this.logInButton.UseVisualStyleBackColor = true;
-			this.logInButton.Click += new System.EventHandler(this.logInButton_Click);
+			logInButton.Font = new Font("Microsoft Sans Serif", 11F);
+			logInButton.Location = new Point(523, 319);
+			logInButton.Margin = new Padding(3, 4, 3, 4);
+			logInButton.Name = "logInButton";
+			logInButton.Size = new Size(102, 46);
+			logInButton.TabIndex = 0;
+			logInButton.Text = "Log In";
+			logInButton.UseVisualStyleBackColor = true;
+			logInButton.Click += logInButton_Click;
 			// 
 			// usernameTextBox
 			// 
-			this.usernameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-			this.usernameTextBox.Location = new System.Drawing.Point(404, 116);
-			this.usernameTextBox.Name = "usernameTextBox";
-			this.usernameTextBox.Size = new System.Drawing.Size(229, 28);
-			this.usernameTextBox.TabIndex = 1;
+			usernameTextBox.Font = new Font("Microsoft Sans Serif", 11F);
+			usernameTextBox.Location = new Point(323, 177);
+			usernameTextBox.Margin = new Padding(3, 4, 3, 4);
+			usernameTextBox.Name = "usernameTextBox";
+			usernameTextBox.Size = new Size(302, 28);
+			usernameTextBox.TabIndex = 1;
 			// 
 			// passwordTextBox
 			// 
-			this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-			this.passwordTextBox.Location = new System.Drawing.Point(404, 189);
-			this.passwordTextBox.Name = "passwordTextBox";
-			this.passwordTextBox.Size = new System.Drawing.Size(229, 28);
-			this.passwordTextBox.TabIndex = 2;
+			passwordTextBox.Font = new Font("Microsoft Sans Serif", 11F);
+			passwordTextBox.Location = new Point(323, 239);
+			passwordTextBox.Margin = new Padding(3, 4, 3, 4);
+			passwordTextBox.Name = "passwordTextBox";
+			passwordTextBox.Size = new Size(302, 28);
+			passwordTextBox.TabIndex = 2;
 			// 
 			// usernameLabel
 			// 
-			this.usernameLabel.AutoSize = true;
-			this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-			this.usernameLabel.Location = new System.Drawing.Point(183, 120);
-			this.usernameLabel.Name = "usernameLabel";
-			this.usernameLabel.Size = new System.Drawing.Size(97, 24);
-			this.usernameLabel.TabIndex = 3;
-			this.usernameLabel.Text = "Username";
+			usernameLabel.AutoSize = true;
+			usernameLabel.Font = new Font("Microsoft Sans Serif", 11F);
+			usernameLabel.Location = new Point(175, 179);
+			usernameLabel.Name = "usernameLabel";
+			usernameLabel.Size = new Size(97, 24);
+			usernameLabel.TabIndex = 3;
+			usernameLabel.Text = "Username";
 			// 
 			// passwordLabel
 			// 
-			this.passwordLabel.AutoSize = true;
-			this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-			this.passwordLabel.Location = new System.Drawing.Point(183, 193);
-			this.passwordLabel.Name = "passwordLabel";
-			this.passwordLabel.Size = new System.Drawing.Size(92, 24);
-			this.passwordLabel.TabIndex = 4;
-			this.passwordLabel.Text = "Password";
+			passwordLabel.AutoSize = true;
+			passwordLabel.Font = new Font("Microsoft Sans Serif", 11F);
+			passwordLabel.Location = new Point(175, 241);
+			passwordLabel.Name = "passwordLabel";
+			passwordLabel.Size = new Size(92, 24);
+			passwordLabel.TabIndex = 4;
+			passwordLabel.Text = "Password";
 			// 
 			// MainForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.passwordLabel);
-			this.Controls.Add(this.usernameLabel);
-			this.Controls.Add(this.passwordTextBox);
-			this.Controls.Add(this.usernameTextBox);
-			this.Controls.Add(this.logInButton);
-			this.Name = "MainForm";
-			this.Text = "App";
-			this.ResumeLayout(false);
-			this.PerformLayout();
-
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = Color.FromArgb(255, 224, 192);
+			ClientSize = new Size(800, 480);
+			Controls.Add(passwordLabel);
+			Controls.Add(usernameLabel);
+			Controls.Add(passwordTextBox);
+			Controls.Add(usernameTextBox);
+			Controls.Add(logInButton);
+			Margin = new Padding(3, 4, 3, 4);
+			Name = "MainForm";
+			Text = "App";
+			ResumeLayout(false);
+			PerformLayout();
 		}
 
 		#endregion
